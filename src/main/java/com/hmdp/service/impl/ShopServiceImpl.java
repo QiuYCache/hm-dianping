2999d9ab07166c25e1400acccb04e1988cab16cb
shunshen <2645303440@qq.com>
2026-04-13 11:05:51 +0800

    实现商户缓存与数据库的双写一致 并解决缓存击穿问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -9,10 +9,13 @@ import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -39,16 +42,35 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop= JSONUtil.toBean(shopJson,Shop.class);
             return Result.ok(shop);
         }
+        if(shopJson!=null){
+            return Result.fail("商户不存在");
+        }
+
         //4.不存在，则根据id查询数据库
         Shop shop = baseMapper.selectById(id);
         if(shop==null){
+            //将空值写入redis
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
             //5.若数据库中不存在该商户,返回失败
             return Result.fail("商户不存在");
         }
         //6.存在，则写入redis
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
         //7.返回
         return Result.ok(shop);
 
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        if(shop.getId()==null){
+            return Result.fail("商户id不能为空");
+        }
+        //更新数据库
+        updateById(shop);
+        //删除redis缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY+shop.getId());
+        return Result.ok();
     }
 }
